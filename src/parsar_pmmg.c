6f65c371f45a0d8bca75ca4a83918fcc17b8d28f
Nikolas Pattakos <nikolaos.pattakos@inria.fr>
2017-04-26 18:13:05 +0200

    eod commit, kind of runs but there are still errors to fix + parsar to finish

--- src/parsar_pmmg.c
+++ src/parsar_pmmg.c
@@ -14,10 +14,10 @@ static void defaultValues( const PMMG_pParMesh parmesh )
 int PMMG_parsar( int argc, char *argv[], PMMG_pParMesh parmesh ) 
 {
   int i = 0;
-  int count = 0;
+
+  int mmgAlloced = argc;
   int mmgArgc = 0;
-  int allocated = argc;
-  char ** mmgArgv = NULL;
+  char** mmgArgv = NULL;
   
   for ( i = 1; i < argc; ++i )
     if ( !strcmp( argv[ i ],"-val" ) )
@@ -27,23 +27,23 @@ int PMMG_parsar( int argc, char *argv[], PMMG_pParMesh parmesh )
 //     handle the -h option by printing the pmmg specifics and then the mmg -h option
 //     handle parmmg specific options and do not add them in the mmgargc/argv list
 //     handle common parmmg/mmg options and add them to the mmgArgc/argv list
-  _MMG5_SAFE_CALLOC( mmgArgv, allocated, char* );
+//  _MMG5_SAFE_CALLOC( mmgArgv, allocated, char* );
 
-  if ( mmgArgc >= allocated ) {
-    allocated *= 2;
-    _MMG5_SAFE_REALLOC( mmgArgv, allocated, char*, "increasing argv array" );
-  }
+//  if ( mmgArgc >= allocated ) {
+//    allocated *= 2;
+//    _MMG5_SAFE_REALLOC( mmgArgv, allocated, char*, "increasing argv array" );
+//  }
 //  strncpy( mmgArgv[mmgArgc], argv[mmgArgc], strlen( argv[mmgArgc] ) + 1 );
 //  ++mmgArgc;
 //     pass the created list to mmg3d_parsar
 
-  _MMG5_SAFE_CALLOC( mmgArgv[0], strlen( argv[0] ) + 1, char );
-  strncpy( mmgArgv[0], argv[0], strlen( argv[0] ) + 1 );
-  mmgArgv[0][strlen( argv[0] )]='\0';
+//  _MMG5_SAFE_CALLOC( mmgArgv[0], strlen( argv[0] ) + 1, char );
+//  strncpy( mmgArgv[0], argv[0], strlen( argv[0] ) + 1 );
+//  mmgArgv[0][strlen( argv[0] )]='\0';
 
   //MMG3D_parsar( mmgArgc, mmgArgv, parmesh->listgrp[0].mesh, parmesh->listgrp[0].met );
   MMG3D_parsar( argc, argv, parmesh->listgrp[0].mesh, parmesh->listgrp[0].met );
 
-  _MMG5_SAFE_FREE( mmgArgv );
+//  _MMG5_SAFE_FREE( mmgArgv );
   return EXIT_SUCCESS;
 }
