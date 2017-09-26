8e6cc533dbf8948682f146c06f67ddee5540be79
Nikolas Pattakos <nikolaos.pattakos@inria.fr>
2017-09-26 16:28:01 +0200

    allow the user to specify location of meshes

--- cmake/testing/pmmg_tests.cmake
+++ cmake/testing/pmmg_tests.cmake
@@ -5,7 +5,7 @@ IF( BUILD_TESTING )
   file( MAKE_DIRECTORY ${CI_DIR} )
   set( CI_DIR_RESULTS  ${CI_DIR}/output )
   file( MAKE_DIRECTORY ${CI_DIR_RESULTS} )
-  set( CI_DIR_INPUTS  ../../TestParMmg )
+  set( CI_DIR_INPUTS  "../../TestParMmg" CACHE PATH "path to test meshes repository" )
 
   foreach( MESH cube_unit-dual_density cube_unit-int_sphere )
     foreach( NP 1 2 4 6 8 )
