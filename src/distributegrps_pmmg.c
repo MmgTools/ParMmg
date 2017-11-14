eb7a4fe37a981d3ed89b1e8f781b23ec11de14d0
Algiane <algiane@gmail.com>
2017-11-14 14:48:12 +0100

    Continue the load balancing skeleton.

--- src/distributegrps_pmmg.c
+++ src/distributegrps_pmmg.c
@@ -20,7 +20,23 @@
  *
  * \return 0 if fail, 1 otherwise
  *
- * Delete parts of the mesh not on the processor.
+ * Send the suitable group to other procs and recieve their groups.
+ *
+ */
+static inline
+int PMMG_mpiexchange_groups(PMMG_pParMesh parmesh,idx_t *part) {
+
+  return 1;
+}
+
+/**
+ * \param parmesh pointer toward the mesh structure.
+ *
+ * \return 0 if fail, 1 otherwise
+ *
+ * Compute the group distribution that load balance the groups over the
+ * processors and send and recieve the groups from the other processors.
+ *
  */
 int PMMG_distribute_groups( PMMG_pParMesh parmesh )
 {
@@ -30,7 +46,8 @@ int PMMG_distribute_groups( PMMG_pParMesh parmesh )
   part = NULL;
   if ( !PMMG_part_parmeshGrps2parmetis(parmesh,part,parmesh->nprocs) ) return 0;
 
-  /** Send the suitable group to other procs and recieve their groups */
+  /** Send the suitable groups to other procs and recieve their groups */
+  if ( !PMMG_mpiexchange_groups(parmesh,part) ) return 0;
 
   return 1;
 }
