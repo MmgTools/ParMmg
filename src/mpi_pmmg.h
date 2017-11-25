7c1bded048f6daa31d36e9b5e22da7a6e2ef9110
Algiane <algiane@gmail.com>
2017-11-25 08:16:56 +0100

    add forgotten file: mpi_pmmg.h

---
+++ src/mpi_pmmg.h
@@ -0,0 +1,60 @@
+#ifndef MPI_PMMG_H
+#define MPI_PMMG_H
+/**
+ * \file mpi_pmmg.h
+ * \brief Mpi tools that are used in different part of parMmg
+ * \author Cécile Dobrzynski (Bx INP/Inria/UBordeaux)
+ * \author Algiane Froehly (Inria/UBordeaux)
+ * \author Nikos Pattakos (Inria)
+ * \version 5
+ * \copyright GNU Lesser General Public License.
+ *
+ */
+#include <mpi.h>
+
+#define MPI_CHECK(func_call,on_failure) do {                            \
+    int mpi_ret_val;                                                    \
+                                                                        \
+    mpi_ret_val = func_call;                                            \
+                                                                        \
+    switch ( mpi_ret_val ) {                                            \
+    case ( MPI_SUCCESS ):                                               \
+      break;                                                            \
+    case ( MPI_ERR_COMM ):                                              \
+      fprintf(stderr," ## Error: %s:%d: Invalid MPI communicator\n.",   \
+              __func__,__LINE__ );                                      \
+      on_failure;                                                       \
+      break;                                                            \
+    case ( MPI_ERR_TYPE ):                                              \
+      fprintf(stderr," ## Error: %s:%d: Invalid MPI datatype\n.",       \
+              __func__,__LINE__ );                                      \
+      on_failure;                                                       \
+      break;                                                            \
+    case ( MPI_ERR_COUNT ):                                             \
+      fprintf(stderr," ## Error: %s:%d: Invalid MPI count argument\n.", \
+              __func__,__LINE__ );                                      \
+      on_failure;                                                       \
+      break;                                                            \
+    case ( MPI_ERR_TAG ):                                               \
+      fprintf(stderr," ## Error: %s:%d: Invalid MPI tag\n.",            \
+              __func__,__LINE__ );                                      \
+      on_failure;                                                       \
+      break;                                                            \
+    case ( MPI_ERR_RANK ):                                              \
+      fprintf(stderr," ## Error: %s:%d: Invalid MPI rank\n.",           \
+              __func__,__LINE__ );                                      \
+      on_failure;                                                       \
+      break;                                                            \
+    case ( MPI_ERR_OTHER ):                                             \
+      fprintf(stderr," ## Error: %s:%d: MPI error.\n.",                 \
+              __func__,__LINE__ );                                      \
+      on_failure;                                                       \
+      break;                                                            \
+    default:                                                            \
+      fprintf(stderr," ## Error: %s:%d: Unexpected MPI error\n.",       \
+              __func__,__LINE__ );                                      \
+      on_failure;                                                       \
+      break;                                                            \
+    }                                                                   \
+  } while(0)
+#endif
