56f383ebb8ca7cad91dc8168a230c00f02a5f6ca
Algiane <algiane@gmail.com>
2018-06-26 18:43:50 +0200

    Change FLT_MAX/MIN in DBL_MAX/MIN in quality_pmmg.c.

--- src/quality_pmmg.c
+++ src/quality_pmmg.c
@@ -24,33 +24,42 @@ int PMMG_outqua( PMMG_pParMesh parmesh )
 
   // Calculate the quality values for local process
   ne = 0;
-  max = FLT_MIN;
+  max = DBL_MIN;
   avg = 0.;
-  min = FLT_MAX;
+  min = DBL_MAX;
   iel = 0;
   good = 0;
   med = 0;
+
   for ( i = 0; i < HIS_SIZE; ++i )
     his[ i ] = 0;
+
   nrid = 0;
   for ( i = 0; i < parmesh->ngrp; ++i ) {
     grp  = &parmesh->listgrp[ i ];
     MMG3D_computeOutqua( grp->mesh, grp->met, &ne_cur, &max_cur, &avg_cur, &min_cur,
                          &iel_cur, &good_cur, &med_cur, his_cur, &nrid_cur );
-    ne += ne_cur;
-    avg += avg_cur;
-    med += med_cur;
+
+
+    ne   += ne_cur;
+    avg  += avg_cur;
+    med  += med_cur;
     good += good_cur;
+
     if ( max_cur > max )
       max = max_cur;
+
     if ( min_cur < min ) {
       min = min_cur;
       iel = iel_cur;
     }
+
     for ( j = 0; j < HIS_SIZE; ++j )
       his[ j ] += his_cur[ j ];
+
     nrid += nrid_cur;
   }
+
 
   // Calculate the quality values for all processes
   MPI_Reduce( &ne, &ne_result, 1, MPI_INT, MPI_SUM, 0, parmesh->comm );
@@ -61,6 +70,7 @@ int PMMG_outqua( PMMG_pParMesh parmesh )
   MPI_Reduce( &min, &min_result, 1, MPI_DOUBLE, MPI_MIN, 0, parmesh->comm );
   MPI_Reduce( his, his_result, HIS_SIZE, MPI_INT, MPI_SUM, 0, parmesh->comm );
   MPI_Reduce( &nrid, &nrid_result, 1, MPI_INT, MPI_SUM, 0, parmesh->comm );
+
   if ( parmesh->myrank == 0 ) {
     grp = &parmesh->listgrp[0];
     if ( MMG3D_displayQualHisto( ne_result, max_result, avg_result, min_result,
