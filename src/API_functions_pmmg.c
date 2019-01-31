ccd0d9497d369893a92989a194462d4489d5f6f4
luca <luca.cirro@gmail.com>
2019-01-31 17:15:48 +0100

    Use parmetis by default.

--- src/API_functions_pmmg.c
+++ src/API_functions_pmmg.c
@@ -121,7 +121,7 @@ void PMMG_Init_parameters(PMMG_pParMesh parmesh,MPI_Comm comm) {
   parmesh->ddebug      = PMMG_NUL;
   parmesh->niter       = PMMG_NITER;
   parmesh->info.fem    = MMG5_FEM;
-  parmesh->info.loadbalancing_mode = PMMG_LOADBALANCING_metis;
+  parmesh->info.loadbalancing_mode = PMMG_LOADBALANCING_parmetis;
   parmesh->info.contiguous_mode = PMMG_CONTIG_DEF;
 
   for ( k=0; k<parmesh->ngrp; ++k ) {
