264c22aa63ef207f852fe83463e5f1156c579c26
luca <luca.cirro@gmail.com>
2019-07-01 10:52:14 +0200

    Increase edge size for sphere refinement test.

--- cmake/testing/pmmg_tests.cmake
+++ cmake/testing/pmmg_tests.cmake
@@ -89,7 +89,7 @@ IF( BUILD_TESTING )
 
     SET ( VAL
       "5"
-      "0.02"
+      "0.05"
       "0.005"
       "1.1"
       "-1"
@@ -99,7 +99,7 @@ IF( BUILD_TESTING )
 
     SET ( NAME
       "v5"
-      "hsiz0.02"
+      "hsiz0.05"
       "hausd0.005"
       "hgrad1.1"
       "nohgrad"
