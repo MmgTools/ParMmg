9c0d48c9a8a612798f24a5e5673b7ffe55d427b4
luca <luca.cirro@gmail.com>
2020-10-02 18:32:18 +0200

    Allow every process to see the whole node memory.

--- src/zaldy_pmmg.c
+++ src/zaldy_pmmg.c
@@ -71,7 +71,7 @@ void PMMG_parmesh_SetMemGloMax( PMMG_pParMesh parmesh )
   }
 
   /** Step 2: Set maximal memory per process depending on the -m option setting */
-  maxAvail = MMG5_memSize()/parmesh->size_shm;
+  maxAvail = MMG5_memSize();
 
   if ( parmesh->info.mem <= 0 ) {
     /* Nos users specifications */
