6418c245c7c78f44c6950bbcd43eb38234d722e4
laetitia-m <laetitia.mottet@gmail.com>
2024-04-04 11:22:15 +0200

    Add file overlap_pmmg.c

---
+++ src/overlap_pmmg.c
@@ -0,0 +1,40 @@
+/* =============================================================================
+**  This file is part of the parmmg software package for parallel tetrahedral
+**  mesh modification.
+**  Copyright (c) Bx INP/Inria/UBordeaux, 2017-
+**
+**  parmmg is free software: you can redistribute it and/or modify it
+**  under the terms of the GNU Lesser General Public License as published
+**  by the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  parmmg is distributed in the hope that it will be useful, but WITHOUT
+**  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+**  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+**  License for more details.
+**
+**  You should have received a copy of the GNU Lesser General Public
+**  License and of the GNU General Public License along with parmmg (in
+**  files COPYING.LESSER and COPYING). If not, see
+**  <http://www.gnu.org/licenses/>. Please read their terms carefully and
+**  use this copy of the parmmg distribution only if you accept them.
+** =============================================================================
+*/
+
+/**
+ * \file isovalue_pmmg.c
+ * \brief Create implicit surface in distribuited mesh.
+ * \author Cécile Dobrzynski (Bx INP/Inria/UBordeaux)
+ * \author Algiane Froehly (InriaSoft)
+ * \author Laetitia Mottet (UBordeaux)
+ * \version 1
+ * \copyright GNU Lesser General Public License.
+ *
+ * Main library functions (parallel remeshing starting from centralized or
+ * distributed data.
+ *
+ */
+
+#include "parmmg.h"
+#include "mmgexterns_private.h"
+
